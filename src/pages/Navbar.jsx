612aef5478de1e9f4069dd51290503df1e897b9f
Nicolas Melo <jaimenicolasmelo@gmail.com>
2024-04-26 14:54:59 -0500

    Favicon and doc title added

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import github from '../assets/img/github.png'
-import movie from '../assets/img/movie.png'
+import movie from '../assets/img/movie.svg'
 
 export const Navbar = () => {
   return (
     <header className='flex justify-between max-w-[90%] mx-auto py-5'>
         <img className='w-12' src={movie} alt="" />
-        <a href="https://github.com/Nimero09" target='_blank' rel="noreferrer"><img className='w-12' src={github} alt="" /></a>
+        <a href="https://github.com/Nimero09" target='_blank' rel="noreferrer"><img className='w-12 fill-white stroke-white	' src={github} alt="Github logo" /></a>
     </header>
   )
 }
